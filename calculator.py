b40ef11a343eeced9cb7702f6659403d834e4fd3
joshposso <j.posso@ufl.edu>
2025-04-09 17:07:52 -0400

    modified calculator p1"

--- calculator.py
+++ calculator.py
@@ -1,7 +1,14 @@
 import math
 #lab 10
 
-def add(a, b): 
+
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    return math.sqrt(a)
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+def add(a, b):
     return a + b
 def subtract(a, b):
     return a - b
@@ -9,7 +16,7 @@ def multiply(a, b):
     return a * b
 def divide(a, b):
     if a == 0:
-        return ValueError
+        raise ValueError
     return b/a
 def logarithm(a, b):
     if a <= 0:
